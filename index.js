6b2fa34b8ac6bdb95ecdcfd76dc7aff51852e4b7
leblanmonster <73737394+leblanmonster@users.noreply.github.com>
2021-01-01 12:42:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ const { PREFIX } = require("./config.json");
 const client = new Client({ disableEveryone: true });
 client.PREFIX = PREFIX;
 const Discord = require('discord.js');
-client.commandsliste = ["kick","ban","avatar","8ball","mute","help","warn","clear","play","tempban","give","money","rank"]
+client.commandsliste = ["kick","ban","avatar","8ball","mute","help","warn","clear","play","tempban","give","money","rank"];
 client.commands = new Collection();
 client.commands.set("kick", require("./commands/kick.js"));
 client.commands.set("ban", require("./commands/ban.js"));
 client.commands.set("avatar", require("./commands/avatar.js"));
-client.commands.set('8ball', require("./commands/8ball.js"))
-client.commands.set('mute', require("./commands/mute.js"))
-client.commands.set('help', require("./commands/help.js"))
-client.commands.set('warn', require("./commands/warn.js"))
-client.commands.set('clear', require("./commands/clear.js"))
-client.commands.set('play', require("./commands/music.js"))
-client.commands.set('tempban', require("./commands/tempmute.js"))
+client.commands.set('8ball', require("./commands/8ball.js"));
+client.commands.set('mute', require("./commands/mute.js"));
+client.commands.set('help', require("./commands/help.js"));
+client.commands.set('warn', require("./commands/warn.js"));
+client.commands.set('clear', require("./commands/clear.js"));
+client.commands.set('play', require("./commands/music.js"));
+client.commands.set('tempban', require("./commands/tempmute.js"));
 client.commands.set("give", require("./commands/give.js"));
 client.commands.set("money", require("./commands/money.js"));
 client.commands.set("rank", require("./commands/rank.js"));
 client.commands.set("lock", require("./commands/lock.js"));
 client.commands.set("unlock", require("./commands/unlock.js"));
 client.on("ready", () => require("./event/ready.js")(client));
-client.on('message', message => require("./event/message.js")(client, message))
-client.on('guildMemberAdd', member => require("./event/guildMemberAdd.js")(client, member))
+client.on('message', message => require("./event/message.js")(client, message));
+client.on('guildMemberAdd', member => require("./event/guildMemberAdd.js")(client, member));
+
 // low dbe
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync');
