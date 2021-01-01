389b2c15c0f10914533f924367c03decb3dbc0d0
leblanmonster <73737394+leblanmonster@users.noreply.github.com>
2021-01-01 12:48:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
 console.log('hello world')
+const { DiscordAPIError } = require("discord.js");
+const { Client, Collection } = require("discord.js")
+const { TOKEN, PREFIX } = require("./config.json");
+const client = new Client({ disableEveryone: true });
+client.PREFIX = PREFIX;
+const Discord = require('discord.js');
+console.log("init discord js end")
